4618a89a0f2f3831ed81ddd48df163113b81175c
Walter Nascimento Barroso <walternascimentobarroso@gmail.com>
2018-05-21 16:08:01 -0400

    [ADD] Adicionando navbar
    Signed-off-by: Walter Nascimento Barroso <walternascimentobarroso@gmail.com>

---
+++ src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent {
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
