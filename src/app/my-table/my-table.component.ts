384c7c3c0843bbf904386a4365142c4505d7b9de
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2018-06-02 19:14:03 -0400

    [ADD] Table translate
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -3,19 +3,24 @@ import { MatPaginator, MatSort } from '@angular/material';
 import { MyTableDataSource } from './my-table-datasource';
 
 @Component({
-  selector: 'my-table',
-  templateUrl: './my-table.component.html',
-  styleUrls: ['./my-table.component.css']
+    selector: 'my-table',
+    templateUrl: './my-table.component.html',
+    styleUrls: ['./my-table.component.css']
 })
 export class MyTableComponent implements OnInit {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  dataSource: MyTableDataSource;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+    dataSource: MyTableDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+    /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+    displayedColumns = ['id', 'name'];
 
-  ngOnInit() {
-    this.dataSource = new MyTableDataSource(this.paginator, this.sort);
-  }
+    ngOnInit() {
+        this.paginator._intl.firstPageLabel = 'Primeira Página';
+        this.paginator._intl.itemsPerPageLabel = 'Registros por página';
+        this.paginator._intl.lastPageLabel = 'Última Página';
+        this.paginator._intl.nextPageLabel = 'Próxima Página';
+        this.paginator._intl.previousPageLabel = 'Página Anterior';
+        this.dataSource = new MyTableDataSource(this.paginator, this.sort);
+    }
 }
