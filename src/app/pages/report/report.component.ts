78c577c95a5a8af1e2a3cbfce2fe29cd4e8ad61d
Walter Nascimento Barroso <walternascimentobarroso@gmail.com>
2018-06-06 10:19:28 -0400

    [ADD] Function printpdf
    Signed-off-by: Walter Nascimento Barroso <walternascimentobarroso@gmail.com>

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,15 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from "@angular/core";
+import * as jsPDF from "jspdf";
+import * as html2canvas from "html2canvas";
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+    selector: "app-report",
+    templateUrl: "./report.component.html",
+    styleUrls: ["./report.component.css"]
 })
 export class ReportComponent implements OnInit {
+    @ViewChild("content") content: ElementRef;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    public downloadPDF() {
+        let pdf = new jsPDF();
 
+        let specialElementHandlers = {
+            "#bypassme": function(element, renderer) {
+                return true;
+            }
+        };
+
+        let content = this.content.nativeElement;
+
+        pdf.fromHTML(content.innerHTML, 15, 15, {
+            width: 190,
+            elementHandlers: specialElementHandlers
+        });
+
+        pdf.save("test.pdf");
+    }
+
+    public export() {
+        const pdf = new jsPDF("p", "pt", "a4");
+
+        html2canvas(document.body).then(canvas => {
+            pdf.fromHTML(document.body, 10, 10, { pagesplit: true }, () => {
+                pdf.save("export.pdf");
+            });
+        });
+    }
+
+    public demoFromHTML() {
+        var pdf = new jsPDF('p', 'pt', 'letter');
+        // source can be HTML-formatted string, or a reference
+        // to an actual DOM element from which the text will be scraped.
+        let source = `
+        <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+        `;
+
+        // we support special element handlers. Register them with jQuery-style
+        // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+        // There is no support for any other type of selectors
+        // (class, of compound) at this time.
+        let specialElementHandlers = {
+            // element with id of "bypass" - jQuery style selector
+            '#bypassme': function (element, renderer) {
+                // true = "handled elsewhere, bypass text extraction"
+                return true
+            }
+        };
+        let margins = {
+            top: 80,
+            bottom: 60,
+            left: 40,
+            width: 522
+        };
+        // all coords and widths are in jsPDF instance's declared units
+        // 'inches' in this case
+        pdf.fromHTML(
+        source, // HTML string or DOM elem ref.
+        margins.left, // x coord
+        margins.top, { // y coord
+            'width': margins.width, // max width of content on PDF
+            'elementHandlers': specialElementHandlers
+        },
+
+        function (dispose) {
+            // dispose: object with X, Y of the last line add to the PDF
+            //          this allow the insertion of new lines after html
+            pdf.save('Test.pdf');
+        }, margins);
+    }
+
+    ngOnInit() {}
 }
