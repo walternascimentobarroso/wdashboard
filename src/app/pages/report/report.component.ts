1473fbe703ec77fc3497a52871108dcde831e377
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2018-06-06 21:42:45 -0400

    [ADD] export to doc
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -14,46 +14,110 @@ export class ReportComponent implements OnInit {
 
     constructor() { }
 
+    public downloadDOCX() {
+        let source = `
+            <table>
+                <tr>
+                <th>#</th>
+                <th>Nome</th>
+                </tr>
+                <tr>
+                <td>1</td>
+                <td>Hydrogen</td>
+                </tr>
+                <tr>
+                <td>2</td>
+                <td>Helium</td>
+                </tr>
+                <tr>
+                <td>3</td>
+                <td>Lithium</td>
+                </tr>
+                <tr>
+                <td>4</td>
+                <td>Beryllium</td>
+                </tr>
+                <tr>
+                <td>5</td>
+                <td>Boron</td>
+                </tr>
+                <tr>
+                <td>6</td>
+                <td>Carbon</td>
+                </tr>
+                <tr>
+                <td>7</td>
+                <td>Nitrogen</td>
+                </tr>
+                <tr>
+                <td>8</td>
+                <td>Oxygen</td>
+                </tr>
+                <tr>
+                <td>9</td>
+                <td>Fluorine</td>
+                </tr>
+                <tr>
+                <td>10</td>
+                <td>Neon</td>
+                </tr>
+            </table>
+        `;
+        var blob = new Blob([source], {
+            type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document;charset=utf-8"
+        });
+        saveAs(blob, "note.docx");
+    }
+
     public downloadXLS() {
         let source = `
         <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
-        `;
+            <tr>
+            <th>#</th>
+            <th>Nome</th>
+            </tr>
+            <tr>
+            <td>1</td>
+            <td>Hydrogen</td>
+            </tr>
+            <tr>
+            <td>2</td>
+            <td>Helium</td>
+            </tr>
+            <tr>
+            <td>3</td>
+            <td>Lithium</td>
+            </tr>
+            <tr>
+            <td>4</td>
+            <td>Beryllium</td>
+            </tr>
+            <tr>
+            <td>5</td>
+            <td>Boron</td>
+            </tr>
+            <tr>
+            <td>6</td>
+            <td>Carbon</td>
+            </tr>
+            <tr>
+            <td>7</td>
+            <td>Nitrogen</td>
+            </tr>
+            <tr>
+            <td>8</td>
+            <td>Oxygen</td>
+            </tr>
+            <tr>
+            <td>9</td>
+            <td>Fluorine</td>
+            </tr>
+            <tr>
+            <td>10</td>
+            <td>Neon</td>
+            </tr>
+        </table>
+    `;
         var blob = new Blob([source], {
             type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
         });
@@ -95,43 +159,52 @@ export class ReportComponent implements OnInit {
         // to an actual DOM element from which the text will be scraped.
         let source = `
         <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
-        `;
+            <tr>
+            <th>#</th>
+            <th>Nome</th>
+            </tr>
+            <tr>
+            <td>1</td>
+            <td>Hydrogen</td>
+            </tr>
+            <tr>
+            <td>2</td>
+            <td>Helium</td>
+            </tr>
+            <tr>
+            <td>3</td>
+            <td>Lithium</td>
+            </tr>
+            <tr>
+            <td>4</td>
+            <td>Beryllium</td>
+            </tr>
+            <tr>
+            <td>5</td>
+            <td>Boron</td>
+            </tr>
+            <tr>
+            <td>6</td>
+            <td>Carbon</td>
+            </tr>
+            <tr>
+            <td>7</td>
+            <td>Nitrogen</td>
+            </tr>
+            <tr>
+            <td>8</td>
+            <td>Oxygen</td>
+            </tr>
+            <tr>
+            <td>9</td>
+            <td>Fluorine</td>
+            </tr>
+            <tr>
+            <td>10</td>
+            <td>Neon</td>
+            </tr>
+        </table>
+    `;
 
         // we support special element handlers. Register them with jQuery-style
         // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
