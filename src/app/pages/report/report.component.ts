1dfcd6462c0ee0dc0190c1d2e6fe1f47350e338d
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2018-06-06 21:23:26 -0400

    [ADD] Export xls
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, ElementRef, OnInit } from "@angular/core";
 import * as jsPDF from "jspdf";
 import * as html2canvas from "html2canvas";
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver/FileSaver';
 
 @Component({
     selector: "app-report",
@@ -10,13 +12,59 @@ import * as html2canvas from "html2canvas";
 export class ReportComponent implements OnInit {
     @ViewChild("content") content: ElementRef;
 
-    constructor() {}
+    constructor() { }
+
+    public downloadXLS() {
+        let source = `
+        <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+        `;
+        var blob = new Blob([source], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+        });
+        saveAs(blob, "Report.xls");
+    }
 
     public downloadPDF() {
         let pdf = new jsPDF();
 
         let specialElementHandlers = {
-            "#bypassme": function(element, renderer) {
+            "#bypassme": function (element, renderer) {
                 return true;
             }
         };
@@ -105,19 +153,19 @@ export class ReportComponent implements OnInit {
         // all coords and widths are in jsPDF instance's declared units
         // 'inches' in this case
         pdf.fromHTML(
-        source, // HTML string or DOM elem ref.
-        margins.left, // x coord
-        margins.top, { // y coord
-            'width': margins.width, // max width of content on PDF
-            'elementHandlers': specialElementHandlers
-        },
+            source, // HTML string or DOM elem ref.
+            margins.left, // x coord
+            margins.top, { // y coord
+                'width': margins.width, // max width of content on PDF
+                'elementHandlers': specialElementHandlers
+            },
 
-        function (dispose) {
-            // dispose: object with X, Y of the last line add to the PDF
-            //          this allow the insertion of new lines after html
-            pdf.save('Test.pdf');
-        }, margins);
+            function (dispose) {
+                // dispose: object with X, Y of the last line add to the PDF
+                //          this allow the insertion of new lines after html
+                pdf.save('Test.pdf');
+            }, margins);
     }
 
-    ngOnInit() {}
+    ngOnInit() { }
 }
