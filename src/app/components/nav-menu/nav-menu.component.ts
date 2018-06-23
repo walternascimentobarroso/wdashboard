0bc7cf4759af48fdb363d8d8efea48d017359ecf
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2018-06-23 19:39:30 -0400

    [ADD] Adicionando funcao de fullscreen
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -8,6 +8,24 @@ import { Observable } from "rxjs";
 import { environment } from "./../../../environments/environment";
 import { AuthService } from "./../../services/auth/auth.service";
 
+declare global {
+    interface Document {
+        msExitFullscreen: any;
+        mozCancelFullScreen: any;
+        mozFullScreenElement: any;
+        msFullscreenElement: any;
+        ALLOW_KEYBOARD_INPUT: any;
+    }
+
+
+    interface HTMLElement {
+        msRequestFullscreen: any;
+        mozRequestFullScreen: any;
+        webkitRequestFullscreen: any;
+    }
+
+}
+
 @Component({
     selector: "nav-menu",
     templateUrl: "./nav-menu.component.html",
@@ -16,11 +34,40 @@ import { AuthService } from "./../../services/auth/auth.service";
 export class NavMenuComponent {
     logo: string = environment.logopath;
     projectname: string = environment.projectname;
+    fullScreenActive: boolean = false;
 
     isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(
         Breakpoints.Handset
     );
     constructor(private breakpointObserver: BreakpointObserver, public authservice: AuthService) { }
+
+    entrarFullScreen() {
+        this.fullScreenActive = true;
+        if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element['ALLOW_KEYBOARD_INPUT']);
+            }
+        }
+    }
+
+    sairFullScreen() {
+        this.fullScreenActive = false;
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
 
     logout() {
         this.authservice.logout();
