799d5a98b98cc7902cce5bb1c31d66d7a304024a
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2018-06-25 17:09:39 -0400

    [ADD] Listando etapa3
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

--- src/app/pages/report-form/report-form.component.ts
+++ src/app/pages/report-form/report-form.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
 import { CrudService } from "../../services/crud/crud.service";
-import { FileService } from "../../services/file/file.service";
 import { ActivatedRoute, Router } from "@angular/router";
-import { Observable } from "rxjs";
-import { finalize } from "rxjs/operators";
 
 @Component({
     selector: "app-report-form",
@@ -18,50 +15,40 @@ export class ReportFormComponent implements OnInit {
     key: any;
     form: any;
 
-    hideUpload: boolean = false;
-    uploadPercent: Observable<number>;
-    profileUrl: Observable<string | null>;
-
     constructor(
         private crudservice: CrudService,
         private route: ActivatedRoute,
-        private router: Router,
-        private fileservice: FileService
+        private router: Router
     ) {
         this.route.params.subscribe(params => {
             this.key = params["key"];
             this.form = new FormGroup({
                 responsible: new FormControl(params["responsible"]),
                 equipment: new FormControl(params["equipment"]),
-                owner: new FormControl(params["owner"])
+                owner: new FormControl(params["owner"]),
+                hometype: new FormControl(params["hometype"]),
+                cep: new FormControl(params["cep"]),
+                number: new FormControl(params["number"]),
+                neighborhood: new FormControl(params["neighborhood"]),
+                street: new FormControl(params["street"]),
+                email: new FormControl(params["email"]),
+                phone: new FormControl(params["phone"])
             });
         });
-
-        this.showFile("dFe2OO1CWb0EL1T.jpeg");
-    }
-
-    uploadFile(event) {
-        this.hideUpload = true;
-        const name = this.fileservice.randomNameFile(
-            15,
-            event.target.files[0].name
-        );
-        const task = this.fileservice.upload(event.target.files[0], name);
-        this.uploadPercent = task.percentageChanges();
-        task.snapshotChanges()
-            .pipe(finalize(() => (this.hideUpload = false)))
-            .subscribe();
-    }
-
-    showFile(name) {
-        this.profileUrl = this.fileservice.show(name);
     }
 
     onSubmit(): void {
         this.form = {
             responsible: this.form.get("responsible").value,
+            cep: this.form.get("cep").value,
+            email: this.form.get("email").value,
             equipment: this.form.get("equipment").value,
-            owner: this.form.get("owner").value
+            hometype: this.form.get("hometype").value,
+            neighborhood: this.form.get("neighborhood").value,
+            number: this.form.get("number").value,
+            owner: this.form.get("owner").value,
+            phone: this.form.get("phone").value,
+            street: this.form.get("street").value
         };
 
         if (this.key) {
