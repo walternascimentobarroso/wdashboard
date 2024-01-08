6a4d837e0ee0f1134c3ecf707980886e71a76f64
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2024-01-08 19:30:52 +0000

    ➕ ADD DEP: Add shadcn-ui
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "@/components/ui/button"
+
 
 export default function Counter({
   dictionary,
@@ -13,14 +15,15 @@ export default function Counter({
   const [count, setCount] = useState(0);
   return (
     <p className="m-4 p-4 border rounded-md bg-green-300 font-bold">
-      Client Component:{" "}
-      <button
-        onClick={() => setCount((n) => n - 1)}
+      Client Component:
+      <Button onClick={() => setCount((n) => n - 1)} variant="secondary">{dictionary.decrement}</Button>
+      {/* <button
+        
         className="bg-green-700 text-white rounded-md m-2 p-2"
       >
-        {dictionary.decrement}
-      </button>{" "}
-      <span className="text-2xl">{count}</span>{" "}
+        
+      </button> */}
+      <span className="text-2xl">{count}</span>
       <button
         onClick={() => setCount((n) => n + 1)}
         className="bg-green-700 text-white rounded-md m-2 p-2"
