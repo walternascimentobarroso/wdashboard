16ae6ea369bcc3f6fa8d4fa199a1d29665633efd
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2025-03-25 21:40:35 +0000

    INIT: Init the project
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

--- src/app/[lang]/products/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Locale } from "@/i18n-config";
-
-export default function Product({
-  params: { lang },
-}: {
-  params: { lang: Locale };
-}) {
-  return <div className="text-4xl">Products: {lang}</div>;
-}
